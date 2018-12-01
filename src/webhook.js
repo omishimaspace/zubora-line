6bbef0a5461070db1c4737d8c85f61765b892ffa
安藤 光昭 <ando@bitriver.jp>
2018-12-01 17:18:16 +0900

    現在のシナリオをDynamoDBに保存するように変更

--- src/webhook.js
+++ src/webhook.js
@@ -5,24 +5,53 @@ const line = require('@line/bot-sdk');
 const lineClient = new line.Client({channelAccessToken: process.env.CHANNEL_ACCES_TOKEN});
 const docClient = new aws.DynamoDB.DocumentClient({region: 'ap-northeast-1'});
 
-const REQUEST_BY_MATERIAL = '食材で選ぶ';
+const REQUEST_BY_FOOD = '食材で選ぶ';
 const REQUEST_BY_CATEGORY = '料理のジャンルから選ぶ';
-const REQUEST_BY_WORK_TIME = '料理時間で選ぶ';
+const REQUEST_BY_TIMES = '料理時間で選ぶ';
 
-const questionForMaterial = {
-  'type': 'text',
-  'text': '使う食材を教えてください',
-};
+const questions = {
+  'food': {
+    'scenario': 1,
+    'message': {
+      'type': 'text',
+      'text': '使う食材を教えてください',
+    }
+  },
+  'category': {
+    'scenario': 2,
+    'message': {
+      'type': 'text',
+      'text': '料理のジャンルを教えてください',
+    }
+  },
+  'times': {
+    'scenario': 3,
+    'message': {
+      'type': 'text',
+      'text': 'どのくらい時間をかけたいですか？',
+    }
+  },
+}
 
-const questionForCategory = {
-  'type': 'text',
-  'text': '今日の気分に合うジャンルを選んでください',
-};
+const setState = (params) => {
+  let item = {
+    userId: params.userId,
+    timestamp: params.timestamp,
+    scenario: params.scenario,
+  };
+  let payload = {
+    TableName: 'line-context',
+    Item: item
+  };
 
-const questionForWorkTime = {
-  'type': 'text',
-  'text': '調理時間を選んでください',
-};
+  docClient.put(payload, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
+}
 
 // AWS Lambda から呼び出される処理
 exports.handler = (event, context, callback) => {
@@ -33,19 +62,27 @@ exports.handler = (event, context, callback) => {
   let response = { statusCode: 200 };
 
   body.events.some(async (lineEvent) => {
-    let message = null;
+    let question = null;
     if (lineEvent.type === 'message' && lineEvent.message && lineEvent.message.type === 'text') {
-      if (lineEvent.message.text === REQUEST_BY_MATERIAL) {
-        message = questionForMaterial;
+      if (lineEvent.message.text === REQUEST_BY_FOOD) {
+        question = questions.food;
       } else if (lineEvent.message.text === REQUEST_BY_CATEGORY) {
-        message = questionForCategory;
-      } else if (lineEvent.message.text === REQUEST_BY_WORK_TIME) {
-        message = questionForWorkTime;
+        question = questions.category;
+      } else if (lineEvent.message.text === REQUEST_BY_TIMES) {
+        question = questions.times;
+      } else {
+        //シナリオから処理を選ぶ
+        
       }
-      if (message) {
+      if (question) {
         console.log(lineEvent.replyToken);
-        console.log(message);
-        await lineClient.replyMessage(lineEvent.replyToken, message);
+        console.log(question);
+        await setState({
+          userId: lineEvent.source.userId,
+          timestamp: lineEvent.timestamp,
+          scenario: question.scenario,
+        });
+        await lineClient.replyMessage(lineEvent.replyToken, question.message);
         return true;
       }
     }
