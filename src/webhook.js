7d9e38c85359c7cc3257ed8ff3967a19ff39e2e1
安藤 光昭 <ando@bitriver.jp>
2018-12-01 23:41:54 +0900

    質問の答えを制御するようにした

--- src/webhook.js
+++ src/webhook.js
@@ -9,23 +9,29 @@ const REQUEST_BY_FOOD = '食材で選ぶ';
 const REQUEST_BY_CATEGORY = '料理のジャンルから選ぶ';
 const REQUEST_BY_TIMES = '料理時間で選ぶ';
 
+const SCENARIO_ID_FOOD = 1;
+const SCENARIO_ID_CATEGORY = 2;
+const SCENARIO_ID_TIMES = 3;
+
+const tableName = 'line-context';
+
 const questions = {
   'food': {
-    'scenario': 1,
+    'scenario': SCENARIO_ID_FOOD,
     'message': {
       'type': 'text',
       'text': '使う食材を教えてください',
     }
   },
   'category': {
-    'scenario': 2,
+    'scenario': SCENARIO_ID_CATEGORY,
     'message': {
       'type': 'text',
       'text': '料理のジャンルを教えてください',
     }
   },
   'times': {
-    'scenario': 3,
+    'scenario': SCENARIO_ID_TIMES,
     'message': {
       'type': 'text',
       'text': 'どのくらい時間をかけたいですか？',
@@ -40,7 +46,7 @@ const setState = (params) => {
     scenario: params.scenario,
   };
   let payload = {
-    TableName: 'line-context',
+    TableName: tableName,
     Item: item
   };
 
@@ -53,6 +59,23 @@ const setState = (params) => {
   });
 }
 
+const getState = async (userId) => {
+  let query = {
+    TableName: tableName,
+    Key: {
+      'userId': userId
+    }
+  };
+
+  let data = null;
+  try {
+    data = await docClient.get(query).promise();
+  } catch (err) {
+    console.log(err);
+  }
+  return data;
+}
+
 // AWS Lambda から呼び出される処理
 exports.handler = (event, context, callback) => {
   console.log(event);
@@ -63,6 +86,7 @@ exports.handler = (event, context, callback) => {
 
   body.events.some(async (lineEvent) => {
     let question = null;
+    let answer = null;
     if (lineEvent.type === 'message' && lineEvent.message && lineEvent.message.type === 'text') {
       if (lineEvent.message.text === REQUEST_BY_FOOD) {
         question = questions.food;
@@ -72,7 +96,23 @@ exports.handler = (event, context, callback) => {
         question = questions.times;
       } else {
         //シナリオから処理を選ぶ
-        
+        let state = await getState(lineEvent.source.userId);
+        console.log('state = ' + state);
+        if (state) {
+          console.log('Current state is: ' + state);
+          if (state.Item.scenario) {
+            if (state.Item.scenario === SCENARIO_ID_FOOD) {
+              answer = '探してます';
+            } else {
+              let number = lineEvent.message.text * 1;
+              if (number > 0) {
+                answer = '探してます';
+              } else {
+                answer = '数字で入力してください';
+              }
+            }
+          }
+        }
       }
       if (question) {
         console.log(lineEvent.replyToken);
@@ -83,6 +123,19 @@ exports.handler = (event, context, callback) => {
           scenario: question.scenario,
         });
         await lineClient.replyMessage(lineEvent.replyToken, question.message);
+        return true;
+      } else if (answer) {
+        console.log(lineEvent.replyToken);
+        console.log(answer);
+        // await setState({
+        //   userId: lineEvent.source.userId,
+        //   timestamp: lineEvent.timestamp,
+        //   scenario: question.scenario,
+        // });
+        await lineClient.replyMessage(lineEvent.replyToken, {
+          'type': 'text',
+          'text': answer,
+        });
         return true;
       }
     }
